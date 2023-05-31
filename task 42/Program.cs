e7605bd08e501274435303fa2f9d41d247ed37ed
Adema <aatygaeva08@gmail.com>
2023-06-01 00:05:40 +0600

    recursiya

---
+++ task 42/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int Binary(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    Binary(n / 2);
+    Console.Write($"{n % 2}");
+    return 0;
+}
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"{Binary(n)}");
+Binary(n);
---
+++ task 45/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+int[] Fill(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-9, 10);
+    }
+    return arr;
+}
+
+System.Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = Fill(length);
+System.Console.WriteLine(string.Join(", ", array));
+int[] copy = new int[length];
+for(int i = 0; i < length; i++) {
+    copy[i] = array[i];
+}
+System.Console.WriteLine(string.Join(", ", copy));
